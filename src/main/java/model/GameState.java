978109cb1b5f32c1b3e304d76f2d834b123f9fc1
Deák Eszter <deakeszter0308@gmail.com>
2024-05-31 23:04:01 +0200

    Set main class in pom.xml to /game/Main and renamed state package to model package

--- src/main/java/model/GameState.java
+++ src/main/java/model/GameState.java
@@ -1,8 +1,11 @@
 package state;
 import puzzle.State;
 import puzzle.solver.BreadthFirstSearch;
+import puzzle.solver.Node;
 
 import java.util.HashSet;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class GameState implements State<Integer>{
@@ -43,8 +46,12 @@ public class GameState implements State<Integer>{
         } else if (isValidMove(figureRow, figureCol - moveLength)) {
             figureCol -= moveLength;
         }
-        // Toggle move length
-        nextMoveIsTwo = !nextMoveIsTwo;
+
+        // Check if the figure lands on a special cell
+        if (board[figureRow][figureCol] == 1) {
+            nextMoveIsTwo = !nextMoveIsTwo;
+        }
+
     }
 
 
@@ -62,7 +69,23 @@ public class GameState implements State<Integer>{
     public State clone() {
         return new GameState(board, figureRow, figureCol, nextMoveIsTwo);
     }
+
     private boolean isValidMove(int row, int col) {
         return row >= 0 && row < board.length && col >= 0 && col < board[0].length && board[row][col] != -1;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(figureRow, figureCol, nextMoveIsTwo);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        GameState that = (GameState) obj;
+        return figureRow == that.figureRow && figureCol == that.figureCol && nextMoveIsTwo == that.nextMoveIsTwo;
+    }
+
+
 }
