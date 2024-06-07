adec54d463a3130d10928e26a86f89e7ec860f87
Deák Eszter <deakeszter0308@gmail.com>
2024-06-08 01:13:22 +0200

    Modified Main.java:Remove initial board configuration

--- src/main/java/solver/Main.java
+++ src/main/java/solver/Main.java
@@ -1,31 +1,20 @@
 package solver;
+import org.tinylog.Logger;
 import puzzle.solver.BreadthFirstSearch;
-import puzzle.solver.Node;
-import state.GameState;
-
-import java.util.Optional;
+import model.Direction;
+import model.GameState;
 
 public class Main {
-    public static void main(String[] args) {
-        int[][] initialBoard = {
-                {0, 0, 0, 0, 1, 0, 0, 0},
-                {0, 0, 1, 0, 0, 0, 1, 0},
-                {0, 0, -1, 0, 0, 0, 0, -1},
-                {0, 0, 1, 0, 1, 0, 0, 0},
-                {1, -1, 0, 1, 0, 0, 0, 1},
-                {0, 0, 0, 1, 0, -1, 1, 0},
-                {0, 0, 1, 0, 0, 0, 0, 1},
-                {1, 0, 0, -1, 0, 0, 0, 0}
-        };
-        GameState initialState = new GameState(initialBoard, 0, 0, true);
-        BreadthFirstSearch<Integer> solver = new BreadthFirstSearch<>();
-        Optional<Node<Integer>> solution = solver.solve(initialState);
 
-        if (solution != null) {
-            System.out.println("Találtunk megoldást!");
-        } else {
-            System.out.println("Nincs megoldás!");
-        }
+    public static void main(String[] args) {
+
+        var bfs = new BreadthFirstSearch<Direction>();
+
+        bfs.solveAndPrintSolution(new GameState());
+
+
+
     }
+
 
 }
