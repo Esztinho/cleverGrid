5e401e5458aa91b79df69cc8dc3ced2e33691457
Deák Eszter <deakeszter0308@gmail.com>
2024-06-08 22:07:10 +0200

    Added moveFigure method to handle figure movement logic based on user input

--- src/main/java/game/GameController.java
+++ src/main/java/game/GameController.java
@@ -1,13 +1,34 @@
 package game;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
+import model.Direction;
 import model.GameState;
+import org.tinylog.Logger;
 
 public class GameController {
     @FXML
     private GridPane grid;
 
     private GameState gameState;
-    
+
+
+    private void moveFigure(Direction direction) {
+        Logger.info("moveFigure");
+        if (gameState.isLegalMove(direction)) {
+            gameState.makeMove(direction);
+            
+            if (gameState.isSolved()) {
+                Logger.info("Puzzle solved!");
+            }
+        } else {
+            Logger.warn("Invalid move:({}, {})", direction.getRowChange(), direction.getColChange());
+        }
+    }
+
+
+
+
+
 }
