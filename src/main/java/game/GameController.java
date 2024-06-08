1cee99108b98d9adfc733e8021c5d03ee19f49af
Deák Eszter <deakeszter0308@gmail.com>
2024-06-08 22:09:32 +0200

    Added updateBoard method to display the figure and special squares on the grid

--- src/main/java/game/GameController.java
+++ src/main/java/game/GameController.java
@@ -2,7 +2,10 @@ package game;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import model.Direction;
 import model.GameState;
 import org.tinylog.Logger;
@@ -18,12 +21,38 @@ public class GameController {
         Logger.info("moveFigure");
         if (gameState.isLegalMove(direction)) {
             gameState.makeMove(direction);
-            
+
+            updateBoard();
             if (gameState.isSolved()) {
                 Logger.info("Puzzle solved!");
             }
         } else {
             Logger.warn("Invalid move:({}, {})", direction.getRowChange(), direction.getColChange());
+        }
+    }
+
+    private void updateBoard() {
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                var node = grid.getChildren().get(row * 8 + col);
+                if (node instanceof StackPane) {
+                    StackPane square = (StackPane) node;
+                    square.getChildren().clear();
+                    if (gameState.getFigureRow() == row && gameState.getFigureCol() == col) {
+                        var imageView = new ImageView(new Image("puzzlegame/npc.png"));
+                        imageView.setFitWidth(square.getWidth());
+                        imageView.setFitHeight(square.getHeight());
+                        imageView.setPreserveRatio(true);
+                        square.getChildren().add(imageView);
+                    } else if (gameState.getBoard()[row][col] == -1) {
+                        square.setStyle("-fx-background-color: black;"); // Fekete mezők
+                    } else if (gameState.getBoard()[row][col] == 1) {
+                        square.setStyle("-fx-background-color: gray;");
+                    } else {
+                        square.setStyle("");
+                    }
+                }
+            }
         }
     }
 
