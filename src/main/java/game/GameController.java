beba561be05693ed216350f719ecb9c2ca02b059
Deák Eszter <deakeszter0308@gmail.com>
2024-06-08 21:47:31 +0200

    Added GameController class for handling game logic and UI interactions

---
+++ src/main/java/game/GameController.java
@@ -0,0 +1,13 @@
+package game;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import model.GameState;
+
+public class GameController {
+    @FXML
+    private GridPane grid;
+
+    private GameState gameState;
+    
+}
